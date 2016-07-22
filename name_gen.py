1d966558314b7ddab65eca998ede527e32afaffb
Brahm Capoor <brahm@stanford.edu>
2016-07-22 14:50:19 +0800

    Fixed name generator to provide suitable simple names

--- name_gen.py
+++ name_gen.py
@@ -21,17 +21,34 @@ def generate_random_word(complex = True):
 
     vowels = sample(VOWELS, letters)
     consonants = sample(CONSONANTS, letters)
-    if not complex:
-        vowels.extend(vowels)
-        consonants.extend(consonants)
     shuffle(vowels)
     shuffle(consonants)
-    return "".join(consonants[i] + vowels[i] for i in range(4))
+
+    if complex:
+        return "".join(consonants[i] + vowels[i] for i in range(4))
+    else:
+        unit_1 = consonants[0] + vowels[0]
+        unit_2 = consonants[1] + vowels[1]
+        n_1 = randint(1,3)
+        n_2 = 4 - n_1
+        syllables = []
+        for i in range(n_1):
+            syllables.append(unit_1)
+        for i in range(n_2):
+            syllables.append(unit_2)
+        shuffle(syllables)
+        return "".join(syllable for syllable in syllables)
+
 
 def generate_pair_name():
+    """
+    Generates a string of names for a pair
+    """
+    
     simple = generate_random_word(False)
     hard = generate_random_word(True)
     return simple + " " + hard
+
 
 def main():
     names = [generate_pair_name() for i in range(16)]
