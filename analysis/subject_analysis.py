e2c40479b61a80eacabaa8e6e4041d3deb265a0c
Brahm Capoor <brahm@stanford.edu>
2016-08-18 15:18:00 +0800

    Turning point detection working

--- analysis/subject_analysis.py
+++ analysis/subject_analysis.py
@@ -4,19 +4,32 @@ from random import shuffle
 
 
 def graph(transparencies_1, transparencies_2):
+    
     x = [i for i in range(1,11)]
     sns.pointplot(x, transparencies_1)
     sns.pointplot(x, transparencies_2)
     plt.show()
 
-def find_average(transparencies_1, transparencies_2):
+def find_turning_points(series):
+
+    turning_points = []
+    last_point = len(series) - 1
+
+    for i, point in enumerate(series):
+        if i != 0 and i != last_point:
+            if (point < series[i - 1] and point < series[i + 1]) or \
+            (point > series[i - 1] and point > series[i + 1]):
+                turning_points.append(point)
+
+    return turning_points
+
 
 
 def main():
     transparencies_1 = [20, 11, 19, 12, 18, 13, 17, 14, 16, 15]
     transparencies_2 = [n - 10 for n in transparencies_1]
-    graph(transparencies_1, transparencies_2)
-    find_average(transparencies_1, transparencies_2)
+    #graph(transparencies_1, transparencies_2)
+    print(find_turning_points(transparencies_1))
 
 if __name__ == '__main__':
     main()
