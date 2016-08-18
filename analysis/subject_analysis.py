a4ca74234e9f7bd50094026a544d24cc4d6f381e
Brahm Capoor <brahm@stanford.edu>
2016-08-18 15:39:21 +0800

    Pulls subject info and graphs it

--- analysis/subject_analysis.py
+++ analysis/subject_analysis.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 from random import shuffle
+import csv
 
 
 def graph(transparencies_1, transparencies_2):
-    
-    x = [i for i in range(1,11)]
+
+    x = [i for i in range(1,41)]
     sns.pointplot(x, transparencies_1)
     sns.pointplot(x, transparencies_2)
     plt.show()
@@ -23,13 +24,30 @@ def find_turning_points(series):
 
     return turning_points
 
+def load_subject_data(subject_number):
+    filename = "../subject logs/subject {}.csv".format(subject_number)
 
+    with open(filename, 'r') as f:
+        reader = csv.reader(f)
+        data = list(reader)[1:]
+
+    transparency_log_1 = [trial[1] for trial in data]
+    transparency_log_2 = [trial[3] for trial in data]
+    transparency_log_3 = [trial[5] for trial in data]
+    transparency_log_4 = [trial[7] for trial in data]
+
+    transparency_log_1 = list(map(lambda x: float(x), transparency_log_1))
+    transparency_log_2 = list(map(lambda x: float(x), transparency_log_2))
+    transparency_log_3 = list(map(lambda x: float(x), transparency_log_3))
+    transparency_log_4 = list(map(lambda x: float(x), transparency_log_4))
+
+    return (transparency_log_1, transparency_log_2)
 
 def main():
-    transparencies_1 = [20, 11, 19, 12, 18, 13, 17, 14, 16, 15]
-    transparencies_2 = [n - 10 for n in transparencies_1]
-    #graph(transparencies_1, transparencies_2)
-    print(find_turning_points(transparencies_1))
+    subject_number = input("Subject number? ")
+    transparency_log_1, transparency_log_2 = load_subject_data(subject_number)
+    graph(transparency_log_1, transparency_log_2)
+    find_turning_points(transparencies_1)
 
 if __name__ == '__main__':
     main()
