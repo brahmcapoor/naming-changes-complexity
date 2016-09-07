905f44847015f1e2c8db3a12f702ddac8dd4fdd1
Brahm Capoor <brahm@stanford.edu>
2016-09-07 14:39:22 +0800

    Finished complying with pep8

--- exp1/experiment_objects.py
+++ exp1/experiment_objects.py
@@ -2,45 +2,48 @@ from psychopy import visual
 
 
 """
-These are the objets used to transfer information through the various stages of the experiment.
-You shouldn't need to touch this at all, so it's not commented. 
+These are the objets used to transfer information through the various stages of
+the experiment.
+You shouldn't need to touch this at all, so it's not commented.
 """
+
+
 class Image():
 
     def __init__(self, path, name):
         self.path = path
         self.name = name
 
-    def stimulus(self, window, position = (0,0), size = 130, transparency = 1):
+    def stimulus(self, window, position=(0, 0), size=130, transparency=1):
 
-        image = visual.ImageStim(win = window,
-                                 image = self.path,
-                                 color = (1,1,1),
-                                 size = [size, size],
-                                 pos = position,
-                                 opacity  = transparency)
+        image = visual.ImageStim(win=window,
+                                 image=self.path,
+                                 color=(1, 1, 1),
+                                 size=[size, size],
+                                 pos=position,
+                                 opacity=transparency)
 
         return image
 
-    def label(self, window, position, fontsize = 50):
+    def label(self, window, position, fontsize=50):
 
-        label = visual.TextStim(win = window,
-                                text = self.name,
-                                pos = position,
-                                alignHoriz = 'center',
-                                alignVert = 'center',
-                                height = fontsize)
+        label = visual.TextStim(win=window,
+                                text=self.name,
+                                pos=position,
+                                alignHoriz='center',
+                                alignVert='center',
+                                height=fontsize)
 
         return label
 
-    def foil(self, window, position = (0,0), size = 200):
+    def foil(self, window, position=(0, 0), size=200):
         foil_path = self.path[:-4] + "_mask.png"
 
-        foil = visual.ImageStim(win = window,
-                                image = foil_path,
-                                color = (1,1,1),
-                                size = [size, size],
-                                pos = position)
+        foil = visual.ImageStim(win=window,
+                                image=foil_path,
+                                color=(1, 1, 1),
+                                size=[size, size],
+                                pos=position)
 
         return foil
 
@@ -53,10 +56,11 @@ class ImagePair():
         self.img_2 = Image(path + "2.png", name_pair[1])
         self.images = [self.img_1, self.img_2]
 
+
 class Trial():
 
     def __init__(self, window, subject_number, round_number, dominant_eye,
-                 image_pair,pair_num):
+                 image_pair, pair_num):
 
         self.window = window
         self.subject_number = subject_number
